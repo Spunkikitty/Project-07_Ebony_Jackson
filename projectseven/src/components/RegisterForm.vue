f7af640e8b0b6361ab7b5cfd5e84ba4e07f9bc9d
Spunkikitty <travelbeanie@gmail.com>
2022-07-22 19:02:43 -0400

    Update B

--- projectseven/src/components/RegisterForm.vue
+++ projectseven/src/components/RegisterForm.vue
@@ -1,19 +1,14 @@
-import Vue from 'vue'; 
-import App from to "./App.vue"; 
 
+ <template>
+  <div  class ="app" id="app">
+  <!--   <img alt="Groupomainia" src="./images/icon-left-font.png">        -->  
+  
 
-  <template>
-  <div id="app">
-    <img alt="Groupomania" src="./image">
-  </div>
-
-
-
-  <div class="form">
+<div class="form">
     <h3>Registration</h3>
     <div class="form-group">
-      <label> Email <input v-model="text" v-if="authen === true" min="10" max="45" placeholder="Employee Email"
-          pattern=".+" required> </label>
+      <label> Email <input v-model="text"  min="10" max="45" placeholder="Employee Email"
+          pattern=".+@groupmainia.com" required> </label>
     </div>
 
   </div>
@@ -21,53 +16,36 @@ import App from to "./App.vue";
     <label>Password <input v-model="text" min="10" max="25" placeholder="password" required></label>
     <button @click="say('Thank You')">Register</button>
   </div>
-
-
-
-
-
-
+  </div>
 
 
 </template>
 
-<script>
 
-import image from "./assets/icon-left-font.png"
-
-export default {
-  methods: {
-    onSubmit() { 
-      console.log('form submitted')
-    }
-
-  }
+<!--<script>
+const app = new vue 
+el: '#app'
+data:{
+  Email: ' '
+  Password:' '
+}; 
 
 
+</script>-->
 
+<style>
+.form { 
+border:10px solid black; 
 }
 
+.form-group{ 
 
-
-
-</script>
-
-
-
-<style scoped>
- .form,
- .form-group {
-   border: black 10px solid inset;
-   border-radius: 25px;
-
- }
-
- .submit {
-   background-color: black;
-   border: red 20px solid;
- }
+  border: 5px solid Red; 
+  background: gray;
+  font-size: large;
+  font-style: normal;
+}
 </style>
-
 
 
 
